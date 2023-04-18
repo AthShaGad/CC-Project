1369cc13306827904662ef2260304c4ae155d33a
linx3273 <ayush96.gupta@gmail.com>
2023-04-18 22:57:47 +0530

    spawning streamlit as a using subprocess for workflow end

--- src/run.py
+++ src/run.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import subprocess
 
 os.chdir(os.path.dirname(sys.argv[0]))
 os.chdir("../")
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[-1] == "--run":
-        os.system("nohup python3 -m streamlit run src/app.py")
+        process = subprocess.Popen(["python3", "-m", "streamlit", "run", "src/app.py"], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     else:
         sys.exit(0)
